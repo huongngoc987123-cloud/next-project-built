26a4a8d40d646b3df9c3afd4fcfbd6a710de3156
v\iet <huongngoc987123@gmail.com>
2026-05-18 19:35:59 -0400

    =

---
+++ app/sign-in/page.tsx
@@ -0,0 +1,14 @@
+import { SignIn } from "@stackframe/stack";
+
+export default function App(){
+    return (
+<div className="min-h-screen  flex items-center justify-center bg-gradient-to-br from-purple-50">
+<div className="max-w-md w-full space-y-8">
+    <SignIn />
+    <link href="./"> xin chafo nah</link>
+</div>
+
+</div>
+
+    )
+}
